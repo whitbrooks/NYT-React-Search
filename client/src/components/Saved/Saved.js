a6975c3032093966f115f1cb6738061da106af69
whitbrooks <whitbrooks@gmail.com>
2018-11-12 16:15:28 -0800

    page renders

---
+++ client/src/components/Saved/Saved.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Saved.css";
+
+const Saved = () => (
+
+    <div className="row">
+        <div className="col-sm-12">
+
+            <div className="card">
+
+                <div className="card-header">
+                    <strong>
+                        <i className="fa fa-table"></i>Saved Articles</strong>
+                </div>
+
+                <div className="card-body" id="article-section">
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Saved;
